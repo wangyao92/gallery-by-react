23b2dff60f9015fae64ee2a8cd23c7f091597396
wangyao <740964655@qq.com>
2016-10-07 23:52:46 +0800

    项目初步搭建

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,35 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片数据
+let imageDatas = require('../data/imageDatas.json');
+
+//通过获取的图片数据中的图片名称，转换为图片路径url
+imageDatas = (function(imageDatasArr){
+  for(let i = 0; i < imageDatasArr.length; i++){
+    let tmpImageData = imageDatasArr[i];
+
+    tmpImageData.imageURL = require('../images/' + tmpImageData.fileName)
+
+    imageDatasArr[i] = tmpImageData;
+  }
+  return imageDatasArr;
+})(imageDatas)
+
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <p></p>
-      </div>
+      <section className = "stage">
+        <section className = "img-sec">
+          
+        </section>
+        <nav className = "controller-nav">
+          
+        </nav>
+      </section>
     );
   }
 }
